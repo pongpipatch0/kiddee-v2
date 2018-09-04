0e167d421e49a9e39d441ba9090f4e6ad2074d70
pongpipatch0 <pongpipatch0@gmail.com>
2018-09-04 13:17:11 +0700

    kiddee default

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const less = require('gulp-less');
+const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('css', function(){
+  return gulp.src('client/templates/*.less')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('js', function(){
+  return gulp.src('client/javascript/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'))
+});
+
+gulp.task('default', ['css', 'js']);
